75a9d9b6107a28ad57915dfdde77626ceb3fe18d
henriquejsilva <henriquejosemartinsdasilva@gmail.com>
2024-03-08 10:23:03 -0300

    Exercicio 2

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,11 @@ Route::get('receber/nome', function(request $request){
     $nome = $request->input('nome');
     return $nome;
     });
+    
+    Route::get('receber/nome/idade', function(request $request){
+        $nome = $request->input('nome');
+        $idade = $request->input('idade');
+        return 'Meu nome é ' . $nome . ', tenho ' . $idade . ' anos';
+        }); 
+    
+
